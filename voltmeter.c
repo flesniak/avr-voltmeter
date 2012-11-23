d373bfe1473d00a6cf70ea159d2fa2e80f7a27f3
Fabian Lesniak <fabian.lesniak@student.kit.edu>
2012-11-23 20:19:52 +0100

    fully working with button

--- voltmeter.c
+++ voltmeter.c
@@ -11,14 +11,14 @@
 #include <avr/sleep.h>
 #include <stdbool.h>
 
-const unsigned char plexdelay = 39; //~200Hz
-const unsigned short adcdelay = 1562; //~5Hz
+const unsigned char plexdelay = 40; //~200Hz
+const unsigned short adcdelay = 800; //~5Hz=1562
 const unsigned short compareValues[2][3] = { {700, 70, 7},
-					    {1500, 150, 15} };
-bool buttonpressed = 0;
+					     {1500, 150, 15} };
+bool lastbtnstate = 1;
 unsigned char currentChannel = 0;
 unsigned char currentSegment = 0;
-unsigned char segment[3] = {0, 0, 0};
+unsigned char currentCode[6] = {0, 0, 0, 0, 0, 0};
 unsigned char segmentCode[11] = { 0b11000000, //0
 				  0b11111001, //1
 				  0b10100100, //2
@@ -32,32 +32,59 @@ unsigned char segmentCode[11] = { 0b11000000, //0
 				  0b11111111 }; //segment off
 
 ISR(TIM0_COMPA_vect) {
-	PORTB |= 0b00000011; //disable all segments
+	PORTB = 0b00000011; //disable all segments
 	switch( currentSegment ) {
 		case 2 : currentSegment = 0;
 			break;
-		case 3 : currentSegment = 0; //implement special actions for 4th "segment"/LEDs
+		/*case 3 : currentSegment++; //implement special actions for 4th "segment"/LEDs
+			DDRA |= 1;
+			PORTA = 0b11110001 | (1<<(ADMUX+2));
 			break;
-		default : currentSegment++;
+		case 4 : currentSegment = 0;
+			PORTA &= ~1;
+			DDRA &= ~1;*/
+		default : PORTA = currentCode[currentSegment*2];
+			PORTB = ~(1 << currentSegment) & 3 | currentCode[currentSegment*2+1];
+			currentSegment++;
 	}
-	PORTA = segmentCode[segment[currentSegment]] << 2;
-	PORTB = segment[currentSegment] >> 6-PB2 & (1<<PB2);
-	PORTB &= 1 << currentSegment;
+	TCNT0 = 0;
 }
 
 ISR(TIM1_COMPA_vect) {
+//void doadc() {
+	if( DDRA & 1 )
+		return;
+	TCCR0B = 0;
+	unsigned char decade, number;
+	DDRB &= ~(1<<PB2); //make PB2 input
+	decade = PORTB; //use decade as savePortB
+	PORTB |= 1<<PB2;
 	ADCSRA |= 1<<ADSC; //start conversion
 	while( ADCSRA & (1<<ADSC) ); //wait for conversion to end
 	unsigned short adc = ADC;
 
-	unsigned char decade;
-	for(decade=0; decade < 3; decade++) {
-		segment[decade] = 0;
-		while( adc > compareValues[currentChannel][decade] ) {
-			segment[decade]++;
+	number = (PINB & (1<<PB2)) + lastbtnstate; //use number as button state tester
+	if( number == 1 ) {
+		ADMUX ^= 1; //toggle ADC channel
+		currentChannel = ADMUX & 1;
+		lastbtnstate = 0;
+	} else if( number == (1<<PB2) )
+		lastbtnstate = 1;
+	PORTB = decade;
+	DDRB |= 1<<PB2;
+
+	for(decade = 0; decade < 3; decade++) {
+		number = 0;
+		while( adc >= compareValues[currentChannel][decade] ) {
+			number++;
 			adc -= compareValues[currentChannel][decade];
 		}
+		currentCode[2*decade] = segmentCode[number] << 2;
+		currentCode[2*decade+1] = segmentCode[number] >> 6 << PB2; //get bit 6, move it to PB2 (segment g)
 	}
+
+	TCCR0B = 5;
+	TCNT1 = 0;
 }
 
 int main() {
@@ -65,8 +92,9 @@ int main() {
 	DDRB = 0b00000111;
 	PORTA = 0b00111111; //disable pullup on pa0+1, deactivate leds
 	PORTB = 0b00000111; //enable pullup on pb2, disable pb3
-	ADMUX = 1<<ADEN;
-	
+	ADCSRA = 1<<ADEN;
+	ADMUX = 0;
+
 	OCR0A = plexdelay; //set compare match value
 	OCR1A = adcdelay;
 	TIMSK0 = 2; //enable compare match interrupt
