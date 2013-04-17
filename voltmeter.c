23a9f0778ff5aad55ae4e14b5a29ca3b3d49315b
Fabian Lesniak <fabian.lesniak@student.kit.edu>
2013-04-17 15:45:19 +0200

    lots of stuff done
    adapted to wrong hardware layout
    both channels 0-14.6V range

--- voltmeter.c
+++ voltmeter.c
@@ -3,21 +3,26 @@
  * PA0-PA1 - Messeingang 1+2, PA0 doppelbelegung leds4
  * PA2-PA7 - 7-Segment-Anzeige pin a-f (ACTIVE LOW!!!)
  * PB0 - 7-Segment-Anzeige pin g (ACTIVE LOW!!!) & Button
- * PB1-PB3 - 7-Segment-Anodentreiber */
+ * PB1-PB3 - 7-Segment-Anodentreiber
+ * Flashen: avrdude -p t24 -c usbasp -U flash:w:voltmeter.hex
+ * Fuses: avrdude -p t24 -c usbasp -U lfuse:w:0xe2:m -U hfuse:w:0x5f:m */
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 #include <stdbool.h>
 
-const unsigned char plexdelay = 32; //~200Hz
-const unsigned short adcdelay = 16; //~5Hz=1562; new style: 25*plexdelay=10Hz
-const unsigned short compareValues[2][3] = { {700, 70, 7},
-					     {1500, 150, 15} };
-bool lastbtnstate = 1;
+const unsigned char plexdelay = 32; //~244Hz = 8MHz/1024/plexdelay
+const unsigned char adcdelay = 16; //~15Hz = plexdelay/adcdelay
+const unsigned short compareValues[2][3] = { {700, 70, 7}, //0..14,3V
+                                             {700, 70, 7} }; //0..14,3V
+//					     {1500, 150, 15} }; //0..6,8V
+
+bool lastbtnstate = 0;
 unsigned char currentChannel = 0;
 unsigned char currentSegment = 0;
 unsigned char currentCode[6] = {0, 0, 0, 0, 0, 0};
+/*                                  1gfedcba
 unsigned char segmentCode[11] = { 0b11000000, //0
 				  0b11111001, //1
 				  0b10100100, //2
@@ -28,14 +33,28 @@ unsigned char segmentCode[11] = { 0b11000000, //0
 				  0b11111000, //7
 				  0b10000000, //8
 				  0b10010000, //9
-				  0b11111111 }; //segment off
+				  0b11111111 }; */ //segment off
+				  //1decbfag
+unsigned char segmentCode[12] = { 0b01111110, //0
+                                  0b00011000, //1
+                                  0b01101011, //2
+                                  0b01011011, //3
+                                  0b00011101, //4
+                                  0b01010111, //5
+                                  0b01110111, //6
+                                  0b00011010, //7
+                                  0b01111111, //8
+                                  0b01011111, //9
+                                  0b00000000, //segment off
+                                  0b11100111 }; //Error 'E'
 
 ISR(TIM0_COMPA_vect) {
 	PORTB = 0b00001110; //disable all segments
 	switch( currentSegment ) {
 		case 3 : currentSegment++; //implement special actions for 4th "segment"/LEDs
-			DDRA = 0b11111101; //make PA0 output
-			PORTA = 0b11110000 | (1<<((ADMUX&1)+2)); //pulls down transistor at PA0 and enables led on PA2/3 depending on ADMUX
+                        PORTA = 0;
+			DDRA = 0b11111001; //make PA0 output, PA2 input for button
+			PORTA = (1<<((ADMUX&1)?7:3)); //pulls down transistor at PA0 and enables led on PA3/7 depending on ADMUX
 			break;
 		case 4 : currentSegment = 0;
 			DDRA = 0b11111100; //restore normal I/O config, PORTA is done below
@@ -50,34 +69,42 @@ void doadc() {
 	TCCR0B = 0;
 	ADCSRA |= 1<<ADSC; //start conversion, afterwards do button stuff to save time
 	unsigned char decade, number;
-	DDRB = ~(1<<PB0); //make PB0 input
-	decade = PORTB; //use decade to save PORTB
-	PORTB = 1<<PB0; //enable pullup
+        /*decade = PORTA; //use decade to save PORTA
+	DDRA &= ~(1<<PA2); //make PA2 input
+	PORTA |= 1<<PA2; //enable pullup on PA2 (where button resides)
 
-	if( PINB & 1 ^ lastbtnstate ) { //if button and saved state differ
-		lastbtnstate = PINB & 1; //save current state
+	if( (PINA >> 3 & 1) ^ lastbtnstate ) { //if button and saved state differ
+		lastbtnstate = !lastbtnstate; //save current state
 		if( !lastbtnstate ) { //if button was pressed right now, toggle adc
 			ADMUX ^= 1;
 			currentChannel = ADMUX & 1;
 		}
 	}
-	PORTB = decade; //restore portb
-	DDRB = 0x0f; //make PB0 output again
+	PORTA = decade; //restore portb
+	DDRA = 0b11111100; //make PB0 output again*/
 
 	while( ADCSRA & (1<<ADSC) ); //wait for conversion to end
 	unsigned short adc = ADC;
 
-	for(decade = 0; decade < 3; decade++) {
-		number = 0;
-		while( adc >= compareValues[currentChannel][decade] ) {
-			number++;
-			adc -= compareValues[currentChannel][decade];
-		}
-		if( decade == 0 && number == 0 )
-			number=10;
-		currentCode[2*decade] = segmentCode[number] << 2;
-		currentCode[2*decade+1] = segmentCode[number] >> 6 & 1; //get bit 6, move it to PB0 (segment g)
-	}
+        if( adc > 1016 ) { //Error threshold
+          currentCode[0] = segmentCode[11] << 2;
+          currentCode[1] = segmentCode[11]>> 6 & 1;
+          currentCode[2] = segmentCode[11] << 2;
+          currentCode[3] = segmentCode[11]>> 6 & 1;
+          currentCode[4] = segmentCode[11] << 2;
+          currentCode[5] = segmentCode[11]>> 6 & 1;
+        } else
+          for(decade = 0; decade < 3; decade++) {
+                  number = 0;
+                  while( adc >= compareValues[currentChannel][decade] ) {
+                          number++;
+                          adc -= compareValues[currentChannel][decade];
+                  }
+                  if( decade == 0 && number == 0 )
+                          number=10; //display nothing instead of zero if U<10V
+                  currentCode[2*decade] = segmentCode[number] << 2;
+                  currentCode[2*decade+1] = segmentCode[number] >> 6 & 1; //get bit 6, move it to PB0 (segment g)
+          }
 
 	TCCR0B = 5;
 }
@@ -100,11 +127,20 @@ int main() {
 	while(1) {
 		sleep_mode();
 		//if( (currentCode[2*currentSegment+1] & (6-PB3)) && !(DDRA & 1) && wait > adcdelay ) {
-		if( wait > adcdelay && !(DDRA & 1) ) {
+                if( !(DDRA & 4) ) {
+                  if( (PINA >> 2 & 1) ^ lastbtnstate ) { //if button and saved state differ
+                    lastbtnstate = !lastbtnstate; //save current state
+                    if( lastbtnstate ) { //if button was pressed right now, toggle adc
+                      ADMUX ^= 1;
+                      currentChannel = ADMUX & 1;
+                    }
+                  }
+                }
+                if( wait > adcdelay && !(DDRA & 1) ) {
 			doadc();
 			wait = 0;
-		} else
-			wait++;
+                } else
+                        wait++;
 	}
 
 	return 0;
